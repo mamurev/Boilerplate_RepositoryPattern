8bc4a72db68dcaeb05076d5ca535bd5f368dac07
Murat Dincer <mrtdncr@gmail.com>
2019-11-11 23:54:02 +0300

    Comment corrected

--- Company.DataAccess/Abstract/IProductDal.cs
+++ Company.DataAccess/Abstract/IProductDal.cs
@@ -5,6 +5,6 @@ namespace Company.DataAccess.Abstract
 {
     public interface IProductDal : IEntityRepository<Product>
     {
-        //Add new methods consists of complex queries here
+        //Add new methods consist of complex queries here
     }
 }
