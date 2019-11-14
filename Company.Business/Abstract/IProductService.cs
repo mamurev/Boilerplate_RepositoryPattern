7bcbd93022869d530cfd4e01f47645475e919c8d
Murat Dincer <mrtdncr@gmail.com>
2019-11-14 23:49:01 +0300

    * Business layer added. * ProductManager.cs added

---
+++ Company.Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Company.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.Business.Abstract
+{
+    public interface IProductService
+    {
+        Product GetProductById(int productId);
+        List<Product> GetList();
+        List<Product> GetListByCategory(int categoryId);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
---
+++ Company.Business/Concrete/ProductManager.cs
@@ -0,0 +1,51 @@
+﻿using Company.Business.Abstract;
+using Company.DataAccess.Abstract;
+using Company.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Company.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        //To prevent tight coupling to any ORM tool, we use ctor injection
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public List<Product> GetList()
+        {
+            return _productDal.GetList().ToList();
+        }
+
+        public List<Product> GetListByCategory(int categoryId)
+        {
+            return _productDal.GetList(filter: p => p.CategoryId == categoryId).ToList();
+        }
+
+        public Product GetProductById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}
