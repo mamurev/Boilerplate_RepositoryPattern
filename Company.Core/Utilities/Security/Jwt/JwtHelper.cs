9c9d9efb4a6d2397be59fec31acc30e2398d40a9
Murat Dincer <mrtdncr@gmail.com>
2019-11-28 00:28:30 +0300

    * JwtHelper finished

--- Company.Core/Utilities/Security/Jwt/JwtHelper.cs
+++ Company.Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -7,6 +7,8 @@ using Company.Core.Entity.Concrete;
 using Company.Core.Utilities.Security.Encryption;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using Company.Core.Extensions;
+using System.Linq;
 
 namespace Company.Core.Utilities.Security.Jwt
 {
@@ -29,16 +31,26 @@ namespace Company.Core.Utilities.Security.Jwt
 
         public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
         {
+            //securityKey : helps to create a token
             var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
             var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
+            var jwt = CreateJwtSecurityToken(_tokenOptions, user, signingCredentials, operationClaims);
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var token = jwtSecurityTokenHandler.WriteToken(jwt);
+            return new AccessToken
+            {
+                Token = token,
+                Expiration = _accessTokenExpiration
+            };
         }
 
-        public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions, User user, SigningCredentials signingCredentials, List<OperationClaim> operationClaims)
+        public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions, User user,
+            SigningCredentials signingCredentials, List<OperationClaim> operationClaims)
         {
-            var jwt = new JwtSecurityToken(
+            return new JwtSecurityToken(
                     issuer: tokenOptions.Issuer,
                     audience: tokenOptions.Audience,
-                    claims: operationClaims,
+                    claims: SetClaims(user, operationClaims),
                     expires: _accessTokenExpiration,
                     notBefore: DateTime.Now,
                     signingCredentials: signingCredentials
@@ -48,7 +60,11 @@ namespace Company.Core.Utilities.Security.Jwt
         private IEnumerable<Claim> SetClaims(User user, List<OperationClaim> operationClaims)
         {
             var claims = new List<Claim>();
-            claims.Add(new Claim(type: "email", value: user.Email));
+            claims.AddNameIdentifier(user.Id.ToString());
+            claims.AddEmail(user.Email);
+            claims.AddName($"{user.FirstName} {user.LastName}");
+            claims.AddRoles(operationClaims.Select(c => c.Name).ToArray());
+            return claims;
         }
     }
 }
